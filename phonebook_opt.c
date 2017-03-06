745895ad25152c1ba7162dbea5a7c6da163dae55
Boopslo <oslophantom@gmail.com>
2017-03-06 18:12:18 +0800

    change data structure of phonebook - add new __PHONTBOOK_ENTRY structure

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,15 +1,28 @@
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #include "phonebook_opt.h"
 
 /* FILL YOUR OWN IMPLEMENTATION HERE! */
 entry *findName(char lastName[], entry *pHead)
 {
-    /* TODO: implement */
+    /* implementation */
+    while (pHead != NULL) {
+        if (strcasecmp(lastName, pHead->lastName) == 0)
+            return pHead;
+        pHead = pHead->pNext;
+    }
     return NULL;
 }
 
 entry *append(char lastName[], entry *e)
 {
-    return NULL;
+    /* implementation */
+    e->pNext = (entry *) malloc(sizeof(entry));
+    e = e->pNext;
+    e->phone = NULL;
+    strcpy(e->lastName, lastName);
+    e->pNext = NULL;
+    return e;
 }
--- phonebook_opt.h
+++ phonebook_opt.h
@@ -5,9 +5,8 @@
 
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
+#define OPT 1
+typedef struct __PHONEBOOK_ENTRY {
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,8 +16,15 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+} phonebook_entry;
+
+
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    phonebook_entry *phone;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
+
 
 entry *findName(char lastName[], entry *pHead);
 entry *append(char lastName[], entry *e);
